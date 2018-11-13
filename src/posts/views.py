d7829575ce9f96fe7d96b8fab62cee1a3c192098
krispea3 <christian.bracher@gmail.com>
2018-11-13 14:58:14 +0800

    removed HttpResponse import. Not used yet

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,5 @@
 """ Render, httpresponse, getobjector404... """
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect #, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
